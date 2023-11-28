6f4084a848683f9fd32639d1707aeed44040125f
jaroslaw-filipiak <info@j-filipiak.pl>
2023-11-28 10:52:27 +0100

    handle edit/update product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,6 +19,106 @@ class ProductController extends Controller
         ], 200);
     }
 
+    public function show($id)
+    {
+
+        $product = Product::find($id);
+        if ($product) {
+
+            return response()->json([
+                'status' => 200,
+                'data' => $product
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Product Not Found',
+                'data' => []
+            ], 404);
+        }
+    }
+
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        if ($product) {
+
+            return response()->json([
+                'status' => 200,
+                'data' => $product
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Product Not Found',
+                'data' => []
+            ], 404);
+        }
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'series' => 'required',
+            'type' => 'required',
+            'distance_code' => 'required',
+            'distance_min' => 'required',
+            'distance_max' => 'required',
+            'photo' => 'required',
+            'name' => 'required',
+            'description' => 'required',
+            'short_name' => 'required',
+            'height_mm' => 'required',
+            'height_inch' => 'required',
+            'packaging' => 'required',
+            'euro_palet' => 'required',
+            'price_net' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'message' => 'Bad Request',
+                'data' => $validator->messages()
+            ], 422);
+        } else {
+
+            $product = Product::find($id);
+
+            if ($product) {
+
+                $product->update([
+                    'series' => $request->series,
+                    'type' => $request->type,
+                    'distance_code' => $request->distance_code,
+                    'distance_min' => $request->distance_min,
+                    'distance_max' => $request->distance_max,
+                    'photo' => $request->photo,
+                    'name' => $request->name,
+                    'description' => $request->description,
+                    'short_name' => $request->short_name,
+                    'height_mm' => $request->height_mm,
+                    'height_inch' => $request->height_inch,
+                    'packaging' => $request->packaging,
+                    'euro_palet' => $request->euro_palet,
+                    'price_net' => $request->price_net,
+
+                ]);
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Product Updated',
+                    'data' => $product
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'Product Not Found',
+                    'data' => []
+                ], 404);
+            }
+        }
+    }
+
     public function store(Request $request)
     {
 
--- routes/api.php
+++ routes/api.php
@@ -26,4 +26,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 */
 
 Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{id}', [ProductController::class, 'show']);
+Route::get('products/{id}/edit', [ProductController::class, 'edit']);
+Route::put('products/{id}/edit', [ProductController::class, 'update']);
 Route::post('product', [ProductController::class, 'store']);
